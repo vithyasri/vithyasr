825f5a94f36fe2646779477c1b5f30ed7f3a1327
vithyasri <52270841+vithyasri@users.noreply.github.com>
2019-07-16 11:18:09 +0530

    Create vithya2.py

---
+++ vithya2.py
@@ -0,0 +1,8 @@
+test_string = "Geeksforgeeks is best Computer Science Portal"
+  
+
+print ("The original string is : " +  test_string) 
+
+res = len(test_string.split()) 
+
+print ("The number of words in string are : " +  str(res))
